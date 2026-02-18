713b8c82f5c336ac7744cf51346ebe9da8974f37
Yahir Klein Quintero Suarez <yahir.kleins@gmail.com>
2026-02-17 21:08:27 -0500

    feat: estructura inicial MVVM para PQRS

---
+++ lib/model/pqrs.dart
@@ -0,0 +1,15 @@
+class PQRS {
+  final String? id;
+  final String tipo;
+  final String descripcion;
+  final DateTime fecha;
+  final String estado;
+
+  PQRS({
+    this.id,
+    required this.tipo,
+    required this.descripcion,
+    required this.fecha,
+    required this.estado,
+  });
+}
