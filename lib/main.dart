5aa8f3fceb23d6ef0971eed269a947accc01e969
Yahir Klein Quintero Suarez <yahir.kleins@gmail.com>
2026-03-10 21:16:54 -0500

    add route for pqrs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../view/pqrs_page.dart';
+import 'view/pqrs_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +10,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PQRSPage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const PQRSPage(),
+        '/pqrs': (context) => const PQRSPage(),
+      },
     );
   }
 }
