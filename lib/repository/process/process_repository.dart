7037c45756fb14c1eba5bf119dd8f6ea59bc5e0a
Yahir Klein Quintero Suarez <yahir.kleins@gmail.com>
2026-05-25 21:28:16 -0500

    feat(process): implement Firestore and syncing repositories

--- lib/repository/process/process_repository.dart
+++ lib/repository/process/process_repository.dart
@@ -1,8 +1,26 @@
 import '../../model/process/process_model.dart';
 
 abstract class ProcessRepository {
+  static ProcessRepository? _instance;
+
+  static ProcessRepository get instance {
+    final i = _instance;
+    if (i == null) {
+      throw StateError(
+        'ProcessRepository.instance no fue inicializado. '
+        'Llámalo en main.dart antes de usar el repositorio.',
+      );
+    }
+    return i;
+  }
+
+  static void setInstance(ProcessRepository repo) {
+    _instance = repo;
+  }
+
   Future<List<ProcessModel>> getProcesses();
   Future<void> addProcess(ProcessModel process);
   Future<void> updateProcess(ProcessModel process);
   Future<void> deleteProcess(String processId);
+  Future<ProcessModel?> getProcessById(String processId);
 }
